df3269880582c56d4557956268e2a1dae02fbf90
aryomitrais <ignatius_AS980@mitrais.com>
2024-05-02 14:04:48 +0700

    feat: create contact entity redux handler

---
+++ src/app/redux/features/contact/contactSlice.ts
@@ -0,0 +1,85 @@
+import { http } from '@/utils/http';
+import { createAppSlice } from '../../createAppSlice';
+import { createAsyncThunk, isAnyOf } from '@reduxjs/toolkit';
+
+export interface Contact {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  photo: string;
+}
+
+export interface ContactSliceState {
+  data: Contact[];
+  isLoading: boolean;
+}
+
+const initialState: ContactSliceState = {
+  data: [],
+  isLoading: false,
+};
+
+export const fetchContact = createAsyncThunk('contacts/fetchAll', async () => {
+  return await http.get<Contact[]>('https://contact.herokuapp.com/contact');
+});
+
+export const createContactAsyncThunk = createAsyncThunk(
+  'contacts/create',
+  async (contact: Contact) =>
+    await http.post(`https://contact.herokuapp.com/contact`, contact)
+);
+
+export const updateContactAsyncThunk = createAsyncThunk(
+  'contacts/update',
+  async (contact: Contact) => {
+    const { id, ...updateContactPayload } = contact;
+    return await http.put(
+      `https://contact.herokuapp.com/contact/${id}`,
+      updateContactPayload
+    );
+  }
+);
+
+export const deleteContactAsyncThunk = createAsyncThunk(
+  'contacts/delete',
+  async (contact: Contact) =>
+    await http.delete(`https://contact.herokuapp.com/contact/${contact.id}`)
+);
+
+export const contactSlice = createAppSlice({
+  name: 'contact',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        isAnyOf(
+          fetchContact.pending,
+          createContactAsyncThunk.pending,
+          updateContactAsyncThunk.pending,
+          deleteContactAsyncThunk.pending
+        ),
+        (state) => {
+          state.isLoading = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          createContactAsyncThunk.fulfilled,
+          updateContactAsyncThunk.fulfilled,
+          deleteContactAsyncThunk.fulfilled,
+          createContactAsyncThunk.rejected,
+          updateContactAsyncThunk.rejected,
+          deleteContactAsyncThunk.rejected
+        ),
+        (state) => {
+          state.isLoading = false;
+        }
+      )
+      .addMatcher(isAnyOf(fetchContact.fulfilled), (state, { payload }) => {
+        state.isLoading = false;
+        state.data = payload.responseBody.data;
+      });
+  },
+});
---
+++ src/app/redux/store/store.ts
@@ -0,0 +1,17 @@
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { contactSlice } from '../features/contact/contactSlice';
+
+const rootReducer = combineSlices(contactSlice);
+export type RootState = ReturnType<typeof rootReducer>;
+export type MakeStore = ReturnType<typeof makeStore>;
+
+export const makeStore = (preloadedState?: Partial<RootState> | object) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+
+export const store = makeStore();
+
+export type AppStore = typeof store;
+export type AppDispatch = AppStore['dispatch'];
