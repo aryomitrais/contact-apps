44bf57dd9fb6cbdc87a89757078a0f4bfe53bb1b
aryomitrais <ignatius_AS980@mitrais.com>
2024-05-02 14:01:17 +0700

    test: create test utility

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,52 @@
+import { AppStore, RootState, makeStore } from '@/app/redux/store/store';
+import { RenderOptions, renderHook } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { PropsWithChildren, ReactElement } from 'react';
+import { Provider } from 'react-redux';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: Partial<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  ui: ReactElement,
+  extendedRenderOptions: ExtendedRenderOptions = {}
+) => {
+  const {
+    preloadedState = {},
+    store = makeStore(preloadedState),
+    ...renderOptions
+  } = extendedRenderOptions;
+
+  const Wrapper = ({ children }: PropsWithChildren) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  return {
+    store,
+    user: userEvent.setup(),
+    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+  };
+};
+
+export const renderHookWithProviders = <Result, Props>(
+  render: (initialProps: Props) => Result,
+  extendedRenderOptions: ExtendedRenderOptions = {}
+) => {
+  const {
+    preloadedState = {},
+    store = makeStore(preloadedState),
+    ...renderOptions
+  } = extendedRenderOptions;
+
+  function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return {
+    store,
+    ...renderHook(render, { wrapper: Wrapper, ...renderOptions }),
+  };
+};
